ff442a477a739e5e80735b43f9f5a75123ef97fa
John Newman <newmanj903@gmail.com>
2018-03-29 20:34:27 +0100

    adding extra information to has

--- directory.rb
+++ directory.rb
@@ -1,83 +1,43 @@
+
 =begin
-In the input_students method the cohort value is hard-coded.
-How can you ask for both the name and the cohort? What if one of the values is empty?
-Can you supply a default value? The input will be given to you as a string? How will you convert it to a symbol?
-What if the user makes a typo?
+Our code only works with the student name and cohort. Add more information: hobbies, country of birth, height, etc
 =end
 
+def input_students
+  puts "Please enter the names of the students, then their hobby and height"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  hobby = gets.chomp
+  height = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november, hobbies: hobby, height: height}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 def print_header
-  puts "The students of Villains Academy"
+  puts "The students of my cohort at Makers Academy"
   puts "-------------"
 end
-
 def print(students)
-  count = 0
-  while count < students.length
-    puts "#{students[count][:name]} (#{students[count][:cohort]} cohort)"
-    count +=1
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort) their favourite hobby is #{student[:hobbies]} and they are #{student[:height]}"
   end
-
 end
 
-def print_footer(students)
-puts "Overall, we have #{students.count} great students"
-end
-
-def input_students
-  #method to see if input is empty and ask for it again
-  def ifempty(name)
-    count = 0
-    while name.empty?
-      if count == 1
-        break
-      end
-      puts "Please enter data or press return to exit"
-      name = gets.chomp
-      count +=  1
-    end
-    name
-  end
-  #method to check date is correct
-  def datecheck(string)
-    if string.empty?
-      return string
-    end
-    until ["January", "Febuary", "March", "April", "May", "June",
-          "July", "August", "September", "October", "November", "December"  ].include?(string.capitalize)
-      puts "That is not a valid date"
-      string = gets.chomp
-    end
-    string.capitalize
-  end
-  #method to get data
-  def getdata
-    puts "Enter a name"
-    name = gets.chomp
-    name = ifempty(name)
-    puts "Enter the date"
-    cohort = gets.chomp
-    cohort = ifempty(cohort)
-    cohort = datecheck(cohort)
-    return [name,cohort]
-  end
-  students = []
-  puts "Please enter the names of the students and then the date they start"
-  puts "To finish, just hit return twice/n"
-
-  array = getdata
-
-  while !array.empty? do
-    students << {name: array[0], cohort: array[1]}
-    puts "Now we have #{students.count} students"
-    getdata
-  end
-
-  students
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
 end
 
 students = input_students
 print_header
 print(students)
 print_footer(students)
-
-hello
