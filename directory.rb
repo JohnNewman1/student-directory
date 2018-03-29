242ebdbbe024781cf53fc45e6e921c074561a46a
John Newman <newmanj903@gmail.com>
2018-03-29 20:39:25 +0100

    using centre method

--- directory.rb
+++ directory.rb
@@ -1,22 +1,19 @@
-
 =begin
-Our code only works with the student name and cohort. Add more information: hobbies, country of birth, height, etc
+Research how the method center() of the String class works. Use it in your code to make the output beautifully aligned.
 =end
 
 def input_students
-  puts "Please enter the names of the students, then their hobby and height"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(50)
+  puts "To finish, just hit return twice".center(50)
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
-  hobby = gets.chomp
-  height = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november, hobbies: hobby, height: height}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students".center(50)
     # get another name from the user
     name = gets.chomp
   end
@@ -24,17 +21,17 @@ def input_students
   students
 end
 def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "-------------"
+  puts "The students of my cohort at Makers Academy".center(50)
+  puts "-------------".center(50)
 end
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort) their favourite hobby is #{student[:hobbies]} and they are #{student[:height]}"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(50)
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(50)
 end
 
 students = input_students
