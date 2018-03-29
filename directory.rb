af64b14c38475303a1905b57a972eb41f63caabf
John Newman <newmanj903@gmail.com>
2018-03-29 12:47:58 +0100

    only prints names that start with s

--- directory.rb
+++ directory.rb
@@ -21,7 +21,9 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
+    if student[:name][0] == "s"
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
