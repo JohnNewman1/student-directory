bd1f1f9a8b7359b43a93d2273924c55c85a32657
John Newman <newmanj903@gmail.com>
2018-03-29 21:05:45 +0100

    cohort exercise for directory.rb

--- directory.rb
+++ directory.rb
@@ -1,37 +1,57 @@
 =begin
-Research how the method center() of the String class works. Use it in your code to make the output beautifully aligned.
+In the input_students method the cohort value is hard-coded.
+How can you ask for both the name and the cohort? What if one of the values is empty?
+Can you supply a default value? The input will be given to you as a string?
+How will you convert it to a symbol? What if the user makes a typo?
 =end
 
+
 def input_students
-  puts "Please enter the names of the students".center(50)
-  puts "To finish, just hit return twice".center(50)
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
+  cohort = gets.chomp
   # while the name is not empty, repeat this code
-  while !name.empty? do
+  while !name.empty? || !cohort.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students".center(50)
+    if name.empty?
+      name = "NoNameGiven"
+    end
+    if cohort.empty?
+      cohort = "NoDateGiven"
+    else #checking for typos
+      until ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July',
+       'August', 'September', 'October', 'November','December'].include?(cohort.capitalize)
+       puts "you made a typo"
+       cohort = gets.chomp
+      end
+    end
+
+
+    students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
+    cohort = gets.chomp
   end
   # return the array of students
   students
 end
 def print_header
-  puts "The students of my cohort at Makers Academy".center(50)
-  puts "-------------".center(50)
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(50)
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students".center(50)
+  puts "Overall, we have #{names.count} great students"
 end
 
 students = input_students
